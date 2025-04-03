7e33195a193a1a38301e4897dd33d2db2b82e03f
ArekHanejko <arekimmobile@gmail.com>
2025-04-03 12:59:14 +0200

    Created project, configured tailwindcss and dockerfile

---
+++ gui/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-[48px] text-navFont">Aplikacja do scrappowania BIP-ów</h1>  
+    </>
+  )
+}
+
+export default App
